914b0ebd5d30b75dbc27ed3f8b2e945a02b38879
husnamaan21 <husnamaan1999@gmail.com>
2021-09-07 22:22:40 +0500

    remaining

--- react-ah/react_learning/src/App.js
+++ react-ah/react_learning/src/App.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
-import Data from './Tasks/Task1/Data';
-import Header from './Tasks/Task1/Header';
-import './Tasks/Task1/main.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import Accordion from './Tasks/react-accordition/accordion';
 
  class App extends Component {
   render() {
+    
     return (
       <>
       <div className="app">
-        <Header />
-        <Data />
+        <Accordion />
+      
+       
       </div>
       </>
     )

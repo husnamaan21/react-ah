dedf12421d93ead771d06a10e5f6800024dccc04
husnamaan21 <husnamaan1999@gmail.com>
2021-09-07 23:11:02 +0500

    done

--- react-ah/react_learning/src/Tasks/react-accordition/accordion.jsx
+++ react-ah/react_learning/src/Tasks/react-accordition/accordion.jsx
@@ -3,20 +3,20 @@ import { faChevronDown, faChevronUp } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './style.css';
 import data from './data';
-import { ToggleOnTwoTone } from '@material-ui/icons';
+
 
 
 export default function Accordion() {
     const [flag, setflag] = useState(null)
  
-    // function toggle(val){
-    //     if(flag == val){
-    //         return setflag(null)
-    //     }
-    //     setflag(val)
+    function toggle(val){
+        if(flag == val){
+            return setflag(null)
+        }
+        setflag(val)
 
 
-    // }
+    }
 
 
     return (
@@ -27,7 +27,7 @@ export default function Accordion() {
                     <div>
                     <div className={flag? "up" :"down" }>
                     <p>{i.title} </p>
-                    <button className="btnz"  onClick={() =>setflag(i.id) }>
+                    <button className="btnz"  onClick={() =>toggle(i.id) }>
                         {flag === i.id? <FontAwesomeIcon icon={faChevronUp } className="up" /> : <FontAwesomeIcon icon={faChevronDown} />}
                     </button>
                 </div>

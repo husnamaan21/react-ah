e9ee53c19a8e520e61a8823e0afa80ebe6d713a9
husnamaan21 <husnamaan1999@gmail.com>
2021-09-08 12:38:14 +0500

    Alhamdulilah done

--- react-ah/react_learning/src/Tasks/react-accordition/accordion.jsx
+++ react-ah/react_learning/src/Tasks/react-accordition/accordion.jsx
@@ -7,16 +7,25 @@ import data from './data';
 
 
 export default function Accordion() {
-    const [flag, setflag] = useState(null)
  
-    function toggle(val){
+    const con = data.map((i,val)=>i.content)
+    const arrange = new Set(con)
+    const con1 = [...arrange]
+    const conn = con1.toString()
+    const con2 = conn.slice(0,69)
+    const [flag, setflag] = useState(null)
+    const [buttonz,setbuttonz]=useState(false)
+    let click = buttonz ? con1 : con2;;
+     function toggle(val){
         if(flag == val){
             return setflag(null)
         }
         setflag(val)
 
 
-    }
+    } 
+  
+
 
 
     return (
@@ -32,9 +41,13 @@ export default function Accordion() {
                     </button>
                 </div>
                 <div>
+                    
                     {flag ===i.id  && <div className="content" >
+                       
+                 
     
-                       <p>{i.content} </p>
+                       <p>{click} </p>
+                       <button  className="btnz" onClick={()=>setbuttonz(!buttonz)}>{buttonz ?  'read less' : "read more"}</button>
     
                     </div> 
                     }

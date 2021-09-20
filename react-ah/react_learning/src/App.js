9d36bea22a9b166ab67f80c612499776db08faa1
husnamaan21 <husnamaan1999@gmail.com>
2021-09-20 21:48:25 +0500

    done

--- react-ah/react_learning/src/App.js
+++ react-ah/react_learning/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Effect from './Tasks/tourism/effect';
+
 // import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 import Tourism from './Tasks/tourism/tourism';
@@ -12,7 +12,7 @@ import Tourism from './Tasks/tourism/tourism';
       <div className="app">
   
         <Tourism />
-        <Effect />
+   
 
       
        
